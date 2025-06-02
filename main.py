c45a056fef5c78b2ec9372d4c2242ce3ab6ea0d2
Amrx9 <amr.khaledt1@gmail.com>
2025-06-02 19:12:50 +0300

    feat: group

--- main.py
+++ main.py
@@ -5,19 +5,28 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
     fps = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH/ 2, SCREEN_HEIGHT / 2)
     dt = 0
+
+    
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        for sprite in drawable:
+            sprite.draw(screen)
+        
         pygame.display.flip()
         dt = fps.tick(60) / 1000
 
